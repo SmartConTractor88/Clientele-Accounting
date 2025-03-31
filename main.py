b473ce71e64754e71b1c5e8233a240445ce91085
SmartConTractor <login.blockchain888@gmail.com>
2025-03-31 11:39:17 +0200

    Store hard-coded data in a .csv file.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
-filepath = "clientele.xlsx"
+filepath = "client_data.csv"
 
-df = pd.read_excel(filepath, sheet_name="2025", index_col="date")
+df = pd.read_csv(filepath, sep=",") 
 
+data = [['02/03/2025','Kendrick Lamar','01743339999','200','Card','Afro taper fade', 'Started writing a new album.']]
+
+new_data = pd.DataFrame(data)
+
+new_data.to_csv(filepath, index=False, mode="a", header=False)
+
+df = pd.read_csv(filepath, sep=",") 
 print(df)
 
 
