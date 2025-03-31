3f962d8aeb754484a45d01d445e97c7b5904abc8
SmartConTractor <login.blockchain888@gmail.com>
2025-03-31 12:14:35 +0200

    Create Add, View, Exit actions.

--- main.py
+++ main.py
@@ -2,15 +2,40 @@ import pandas as pd
 
 filepath = "client_data.csv"
 
-df = pd.read_csv(filepath, sep=",") 
+while True:
+    
+    user_action = input("Add / View / Exit: ").upper()
 
-data = [['02/03/2025','Kendrick Lamar','01743339999','200','Card','Afro taper fade', 'Started writing a new album.']]
+    if "ADD" in user_action:
 
-new_data = pd.DataFrame(data)
+        df = pd.read_csv(filepath, sep=",") 
 
-new_data.to_csv(filepath, index=False, mode="a", header=False)
+        date = input("Date (dd/mm/yyyy): ")
+        name = input("Client name: ").strip().title()
+        contact = input("Contact: ")
+        revenue = input("Revenue: ")
+        paid_with = input("Means of payment: ").strip().title()
+        service_type = input("Type of haircut: ").strip().title()
+        note = input("Note: ")
 
-df = pd.read_csv(filepath, sep=",") 
-print(df)
+        data = [[date, name, contact, revenue, paid_with, service_type, note]]
 
+        new_data = pd.DataFrame(data)
+        new_data.to_csv(filepath, index=True, mode="a", header=False)
+
+        df = pd.read_csv(filepath, sep=",") 
+        print(f"Client {name} added successfully.")
+        print("**UPDATED DATA**")
+        print(df)
+
+    if "VIEW" in user_action:
+
+        print("**CURRENT DATA**")
+        df = pd.read_csv(filepath, sep=",") 
+        print(df)
+
+    if "EXIT" in user_action:
+
+        print("Exit successfull")
+        break
 
